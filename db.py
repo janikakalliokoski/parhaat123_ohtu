3015897b775569802d0433e7a1e878f5e8d91072
Oskari Heinonen <oskari.l.heinonen@helsinki.fi>
2022-11-29 14:02:37 +0200

    bugfix local run

--- db.py
+++ db.py
@@ -2,5 +2,5 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("://", "ql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("SQLALCHEMY_DATABASE_URI")
 db = SQLAlchemy(app)
